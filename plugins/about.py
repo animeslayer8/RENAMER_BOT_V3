424f278259ac849d16ae7651516849407d3c5226
animeslayer8 <126319219+animeslayer8@users.noreply.github.com>
2023-04-11 19:16:30 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','6072652725:AAHUqLyazJlWpmMXCxyz1lD-EKNFHw43EFo')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
@@ -10,4 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @Kuski_rename_7_bot\nCreater :- @Katsuki_warlord\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Heroku\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Total User:- {total_user()}\nBot :- @Rename_kuski_2_bot\nCreater :- @Katsuki_warlord\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Heroku\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
