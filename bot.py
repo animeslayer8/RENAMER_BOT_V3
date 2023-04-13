555fedf23a6c22165aa440e3fc3ccf3a919086c5
Katsuki <126319219+animeslayer8@users.noreply.github.com>
2023-04-13 06:38:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,9 @@ API_HASH = os.environ.get("API_HASH", "304228f1dc7b9c86ef1c8d83cdc5da85")
 
 STRING = os.environ.get("STRING", "BQBtVa4ASKZEjK1fJTlgpFth35pgaRIp_0NhCDM7W7vq7ajkn-K5q1NjVHjA9owehPd1w04f5Kut8F9ZfTTqDpHSnzObARoNwlWEyFP1EuE_BvN5H7MZkpSTJxSEbAMmwJa9Z2OkE42z4a0ehcpZsnhDlOPmgrO8l7gzus8nWkKwX1g719fLFXm1lpfnHsrZRM8IRGsnvMNqewumoxLzDfQmANwPJFEWFhemfSjXKyA7rIyqr1y-YhDiAw7U1xBIbd2yC-WgGAoAxbj7nckXN3ZLxuzoWy5nsffhOg3t8i2JFdACNlvNp9gSLd_U11X7wR8FOtmnqkxqKu6a7F6XmVaOCh5uQAAAAAFmd66DAA")
 
+ADMIN = os.environ.get(" ADMIN_ID","6014086787")
+
+
 
 bot = Client(
 
